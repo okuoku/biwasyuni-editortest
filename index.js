8a7fe9e5866a983f7df8f5c823e676839c11a1ad
okuoku <mjt@cltn.org>
2018-08-04 19:27:12 +0900

    Switch to React

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-require('./node_modules/bootstrap');
-var m = require('./node_modules/mithril');
+var r = require('react');
+var d = require('react-dom');
+var createReactClass = require('create-react-class');
 var biwasyuni = require('./biwasyuni/biwasyuni_core.js');
 var biwasloader = require('./loader_biwasyuni.js');
 
@@ -7,7 +8,8 @@ var root = document.body;
 
 var async_loaders = {
     prosemirror: import('./loader_prosemirror.js'),
-    browserfs: import('./node_modules/browserfs')
+    browserfs: import('./node_modules/browserfs'),
+    materialui: import('@material-ui/core/umd/material-ui.production.min.js')
 };
 
 var js_load_async = function(name, cb){
@@ -16,14 +18,17 @@ var js_load_async = function(name, cb){
 
 var loadfs = biwasloader.loadfs("/");
 
+var e = r.createElement;
+
 biwasyuni.switch_console_output(); // Use console.log
-biwasyuni.add_module("m", m);
+biwasyuni.add_module("e", e);
+biwasyuni.add_module("createReactClass", createReactClass);
 biwasyuni.add_module("fs", loadfs); // FIXME: ???
 biwasyuni.add_module("js-load-async", js_load_async);
 biwasyuni.add_module("document-root", root);
 biwasyuni.set_current_fs(loadfs);
 
-m.render(root, "Hello."); // debug
+d.render(e("div", null, "Hello."), document.getElementById('root')); // debug
 
 biwasyuni.run("(load \"boot.scm\")", 
               function(res){ console.log("init done.", res); },
