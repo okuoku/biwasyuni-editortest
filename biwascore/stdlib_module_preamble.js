3f33e047f63a6764085fa0995ba6421f69820627
okuoku <mjt@cltn.org>
2018-08-09 03:38:48 +0900

    Add stdlib modules

---
+++ biwascore/stdlib_module_preamble.js
@@ -0,0 +1,11 @@
+// biwascore: BiwaScheme fork for app embedding 
+//
+// See LICENSE.biwascheme for the original license terms
+
+var _ = require("underscore");
+var _str = require("underscore.string");
+
+_.str = _str;
+
+module.exports = function(BiwaScheme){
+    var bs = BiwaScheme;
