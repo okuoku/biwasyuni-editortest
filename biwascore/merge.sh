80f09b0600bd0be7dda1734936582a476c9b7083
okuoku <mjt@cltn.org>
2018-08-09 03:50:14 +0900

    Add stdlib generation

--- biwascore/merge.sh
+++ biwascore/merge.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
+# Core
 cat core_module_preamble.js src_header/version.js src_header/header.js \
     src_system/class.js \
     src_system/_writer.js \
@@ -23,3 +25,10 @@ cat core_module_preamble.js src_header/version.js src_header/header.js \
     src_system/promise.js \
     src_stdlib/infra.js \
     core_module_postamble.js > gen/biwascore.js
+
+# STDLIB
+cat stdlib_module_preamble.js src_stdlib/extra_lib.js stdlib_module_postamble.js > gen/extra_lib.js
+cat stdlib_module_preamble.js src_stdlib/js_interface.js stdlib_module_postamble.js > gen/js_interface.js
+cat stdlib_module_preamble.js src_stdlib/node_functions.js stdlib_module_postamble.js > gen/node_functions.js
+cat stdlib_module_preamble.js src_stdlib/r6rs_lib.js stdlib_module_postamble.js > gen/r6rs_lib.js
+cat stdlib_module_preamble.js src_stdlib/srfi.js stdlib_module_postamble.js > gen/srfi.js
